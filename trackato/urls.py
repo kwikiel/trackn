82104116f333b913f2daa1f6992bd2d806abe6d3
Kacper Wikieł <k.wikiel@gmail.com>
2017-04-24 05:02:47 +0200

    Create basic page

--- trackato/urls.py
+++ trackato/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from notebook import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^index/', views.index)
 ]
